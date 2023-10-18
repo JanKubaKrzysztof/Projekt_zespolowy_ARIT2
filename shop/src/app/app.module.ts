904c320e818b5fac0b8001c53d03ed71ba0adfcc
Jakub Koscielak <kubakosc2000@gmail.com>
2023-10-18 21:24:04 +0200

    add firebase

--- shop/src/app/app.module.ts
+++ shop/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {  environment } from "../enviroment/enviroment";
+import {  AngularFireModule } from "@angular/fire/compat";
+import {  AngularFireAuthModule } from "@angular/fire/compat/auth";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
     BrowserModule
   ],
   providers: [],
